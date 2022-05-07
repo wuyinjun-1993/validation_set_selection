1bba84835d08d7590aa2a3ef50eb3fd8c773c8fe
thuwuyinjun <wuyinjun@seas.upenn.edu>
2022-05-07 12:42:53 -0400

    update for cifar100

--- src/common/utils.py
+++ src/common/utils.py
@@ -80,20 +80,21 @@ def obtain_optimizer_scheduler(args, net, start_epoch = 0):
                     scheduler = None
         else:
             if args.dataset == 'cifar100':
-                optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+                optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4, nesterov=True)
                 optimizer.param_groups[0]['initial_lr'] = args.lr
                 if args.do_train:
-                    mile_stones_epochs = [100, 200]
+                    # mile_stones_epochs = [100, 200]
+                    mile_stones_epochs = [150, 225]
                     # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                     #                                             milestones=mile_stones_epochs, last_epoch=start_epoch-1, gamma = 0.1)#torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-                    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=mile_stones_epochs, gamma=0.1) #learning rate decay
+                    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=mile_stones_epochs, gamma=0.2) #learning rate decay
                 else:
                     if args.use_pretrained_model:
-                        mile_stones_epochs = [100, 200]
+                        mile_stones_epochs = [150, 225]
                     else:
-                        mile_stones_epochs = [100, 200]
+                        mile_stones_epochs = [150, 225]
                     if args.lr_decay:
-                        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=mile_stones_epochs, last_epoch=start_epoch-1, gamma = 0.1)#torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+                        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,milestones=mile_stones_epochs, last_epoch=start_epoch-1, gamma = 0.2)#torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
                     else:
                         scheduler = None
             else:
