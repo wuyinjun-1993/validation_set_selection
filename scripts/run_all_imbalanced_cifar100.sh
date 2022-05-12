b55b5153e74c1bc11034615dd428b4af1f8e8a68
Adam Stein <steinad@seas.upenn.edu>
2022-05-12 17:20:12 -0400

    Fix port issue with script

--- scripts/run_all_imbalanced_cifar100.sh
+++ scripts/run_all_imbalanced_cifar100.sh
@@ -3,12 +3,12 @@ res_dir=$2
 
 echo Running BaseModel training
 CUDA_VISIBLE_DEVICES=0 ./train_basemodel.sh cifar100 imbalanced 0.005 0.1 10031 ${data_dir} ${res_dir} &
-CUDA_VISIBLE_DEVICES=1 ./train_basemodel.sh cifar100 imbalanced 0.01 0.1 10031 ${data_dir} ${res_dir} &
-CUDA_VISIBLE_DEVICES=2 ./train_basemodel.sh cifar100 imbalanced 0.02 0.1 10031 ${data_dir} ${res_dir} &
-CUDA_VISIBLE_DEVICES=3 ./train_basemodel.sh cifar100 imbalanced 0.05 0.1 10031 ${data_dir} ${res_dir} &
+CUDA_VISIBLE_DEVICES=1 ./train_basemodel.sh cifar100 imbalanced 0.01 0.1 10032 ${data_dir} ${res_dir} &
+CUDA_VISIBLE_DEVICES=2 ./train_basemodel.sh cifar100 imbalanced 0.02 0.1 10033 ${data_dir} ${res_dir} &
+CUDA_VISIBLE_DEVICES=3 ./train_basemodel.sh cifar100 imbalanced 0.05 0.1 10034 ${data_dir} ${res_dir} &
 wait
 CUDA_VISIBLE_DEVICES=0 ./train_basemodel.sh cifar100 imbalanced 0.1 0.1 10031 ${data_dir} ${res_dir} &
-CUDA_VISIBLE_DEVICES=1 ./train_basemodel.sh cifar100 imbalanced 1 0.1 10031 ${data_dir} ${res_dir} &
+CUDA_VISIBLE_DEVICES=1 ./train_basemodel.sh cifar100 imbalanced 1 0.1 10032 ${data_dir} ${res_dir} &
 wait
 
 echo Running imbalanced 200 experiments
