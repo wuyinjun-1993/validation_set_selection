4df09afb8aef3c6e4e9aa0afdc50f05f63e3fe5e
thuwuyinjun <wuyinjun@seas.upenn.edu>
2022-07-28 11:43:11 -0400

    update

---
+++ scripts/config_retina_rand_noise_rate_0.4_cluster_method_two.sh
@@ -0,0 +1,27 @@
+err_label_ratio=0.4
+
+dataset_name=retina
+data_dir="/data1/wuyinjun/valid_set_selections/retina_data/"
+save_path_root_dir="/data1/wuyinjun/valid_set_selections/retina_data/"
+output_dir="/data1/wuyinjun/valid_set_selections/retina_data/"
+gpu_ids=3
+#total_valid_ratio=$6
+repeat_times=2
+port_num=10013
+meta_lr=10
+lr=0.001
+batch_size=128
+test_batch_size=128
+epochs=200
+#cached_model_name=${14}
+add_valid_in_training_set=true
+lr_decay=true
+warm_up_valid_count=10
+model_prov_period=20
+
+
+valid_ratio_each_run=100 #$(( total_valid_ratio / repeat_times ))
+bias_flip=false
+method="cluster_method_two"
+total_valid_sample_count=100
+
---
+++ scripts/sequential_label_valid_set_trec_low_data.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+trap "exit" INT
+
+
+
+err_label_ratio=45
+
+dataset_name=$1
+data_dir=$2
+save_path_root_dir=$3
+output_dir=$4
+gpu_ids=$5
+#total_valid_ratio=$6
+repeat_times=$7
+port_num=$8
+meta_lr=$9
+lr=${10}
+batch_size=${11}
+test_batch_size=${12}
+epochs=${13}
+#cached_model_name=${14}
+add_valid_in_training_set=${14}
+lr_decay=${15}
+
+valid_ratio_each_run=$6 #$(( total_valid_ratio / repeat_times ))
+
+save_path_prefix=${save_path_root_dir}/low_data_${err_label_ratio}_valid_select
+
+
+total_valid_sample_count=12
+
+export CUDA_VISIBLE_DEVICES=${gpu_ids}
+echo CUDA_VISIBLE_DEVICES::${CUDA_VISIBLE_DEVICES}
+
+echo "initial cleaning"
+
+cd ../src/main/
+
+#-cluster_method_three --cosin_dist --weight_by_norm --replace --use_model_prov --model_prov_period 20 --total_valid_sample_count ${total_valid_sample_count}
+add_valid_in_training_flag="--cluster_method_two --cosin_dist  --replace --use_model_prov --model_prov_period 20 --total_valid_sample_count ${total_valid_sample_count}"
+lr_decay_flag="--use_pretrained_model"
+
+<<cmd
+if (( add_valid_in_training_set == true ))
+then
+        add_valid_in_training_flag="--cluster_method_two"
+fi
+
+lr_decay_flag=""
+if (( lr_decay == true  ))
+then
+	lr_decay_flag="--lr_decay"
+fi
+
+echo "add_valid_in_training_flag: ${add_valid_in_training_flag}"
+cmd
+
+
+
+exe_cmd="python -m torch.distributed.launch \
+  --nproc_per_node 1 \
+  --master_port ${port_num} \
+  main_train.py \
+  --nce-t 0.07 \
+  --nce-k 200 \
+  --data_dir ${data_dir} \
+  --dataset ${dataset_name} \
+  --valid_count ${valid_ratio_each_run} \
+  --meta_lr ${meta_lr} \
+  --low_data \
+  --low_data_num_samples_per_class ${err_label_ratio} \
+  --save_path ${save_path_prefix}_do_train/ \
+  --cuda \
+  --lr ${lr} \
+  --batch_size ${batch_size} \
+  --test_batch_size ${test_batch_size} \
+  --epochs ${epochs} \
+  --do_train"
+
+
+output_file_name=${output_dir}/output_${dataset_name}_low_data_${err_label_ratio}_do_train_0.txt
+
+
+echo "${exe_cmd} > ${output_file_name}"
+
+
+${exe_cmd} > ${output_file_name} 2>&1
+
+
+exe_cmd="python -m torch.distributed.launch \
+  --nproc_per_node 1 \
+  --master_port ${port_num} \
+  main_train.py \
+  --load_dataset \
+  --nce-k 200 \
+  --data_dir ${data_dir} \
+  --dataset ${dataset_name} \
+  --valid_count ${valid_ratio_each_run} \
+  --meta_lr 5 \
+  --select_valid_set \
+  --low_data \
+  --low_data_num_samples_per_class ${err_label_ratio} \
+  --save_path ${save_path_prefix}_valid_select_0/ \
+  --prev_save_path ${save_path_prefix}_do_train/ \
+  --cuda \
+  --lr ${lr} \
+  --batch_size ${batch_size} \
+  --test_batch_size ${test_batch_size} \
+  --epochs ${epochs} \
+  ${add_valid_in_training_flag} \
+  ${lr_decay_flag}"
+
+
+output_file_name=${output_dir}/output_${dataset_name}_low_data_${err_label_ratio}_valid_select_0.txt
+
+echo "${exe_cmd} > ${output_file_name}"
+
+${exe_cmd} > ${output_file_name} 2>&1 
+
+mkdir ${save_path_prefix}_no_reweighting_seq_select_0/
+
+#cp ${save_path_prefix}_seq_select_0/* ${save_path_prefix}_no_reweighting_seq_select_0/
+
+
+echo "add_valid_in_training_flag: ${add_valid_in_training_flag}"
+
+
+#for k in {1..${repeat_times}}
+for (( k=1; k<=repeat_times; k++ ))
+do
+
+	exe_cmd="python -m torch.distributed.launch \
+    --nproc_per_node 1 \
+    --master_port ${port_num} \
+    main_train.py \
+    --load_dataset \
+    --select_valid_set \
+    --continue_label \
+    --load_cached_weights \
+    --cached_sample_weights_name cached_sample_weights \
+    --nce-t 0.07 \
+    --nce-k 200 \
+    --data_dir ${data_dir} \
+    --dataset ${dataset_name} \
+    --valid_count ${valid_ratio_each_run} \
+    --meta_lr ${meta_lr} \
+    --not_save_dataset \
+    --low_data \
+    --low_data_num_samples_per_class ${err_label_ratio} \
+    --save_path ${save_path_prefix}_valid_select_$k/ \
+    --prev_save_path ${save_path_prefix}_valid_select_$(( k - 1 ))/ \
+    --cuda \
+    --lr ${lr} \
+    --batch_size ${batch_size} \
+    --test_batch_size ${test_batch_size} \
+    --epochs ${epochs} \
+    ${add_valid_in_training_flag} \
+	${lr_decay_flag}"
+
+	output_file_name=${output_dir}/output_${dataset_name}_low_data_${err_label_ratio}_valid_select_seq_select_$k.txt
+
+	echo "${exe_cmd} > ${output_file_name}"
+	
+	${exe_cmd} > ${output_file_name} 2>&1 
+	
+done
+
+
+
