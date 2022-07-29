9750f3bfb1c514970fc47a939936ed515918f398
thuwuyinjun <wuyinjun@seas.upenn.edu>
2022-07-28 22:08:35 -0400

    update

--- scripts/config_cifar100_rand_noise_rate_0.4_cluster_method_three.sh
+++ scripts/config_cifar100_rand_noise_rate_0.4_cluster_method_three.sh
@@ -2,7 +2,7 @@ err_label_ratio=0.4
 
 dataset_name=cifar100
 data_dir="/data1/wuyinjun/valid_set_selections/cifar100_4/"
-save_path_root_dir="/data2/wuyinjun/valid_set_selections/cifar100_4/"
+save_path_root_dir="/data1/wuyinjun/valid_set_selections/cifar100_4/"
 output_dir="/data1/wuyinjun/valid_set_selections/cifar100_4/"
 gpu_ids=3
 #total_valid_ratio=$6
