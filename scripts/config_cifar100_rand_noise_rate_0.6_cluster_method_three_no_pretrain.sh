404e024c26b1075a76a1136d874b57fee0620789
thuwuyinjun <wuyinjun@seas.upenn.edu>
2022-10-20 21:48:24 -0400

    update

--- scripts/config_cifar100_rand_noise_rate_0.6_cluster_method_three_no_pretrain.sh
+++ scripts/config_cifar100_rand_noise_rate_0.6_cluster_method_three_no_pretrain.sh
@@ -1,13 +1,13 @@
 err_label_ratio=0.6
 
 dataset_name=cifar100
-data_dir="/data6/wuyinjun/valid_set_selections/cifar100_1/"
-save_path_root_dir="/data6/wuyinjun/valid_set_selections/cifar100_1/"
-output_dir="/data6/wuyinjun/valid_set_selections/cifar100_1/"
-gpu_ids=3
+data_dir="/data6/wuyinjun/valid_set_selections/cifar100_2/"
+save_path_root_dir="/data6/wuyinjun/valid_set_selections/cifar100_2/"
+output_dir="/data6/wuyinjun/valid_set_selections/cifar100_2/"
+gpu_ids=0
 #total_valid_ratio=$6
 repeat_times=10
-port_num=10353
+port_num=10330
 meta_lr=30
 lr=0.1
 batch_size=128
@@ -16,12 +16,12 @@ epochs=150
 #cached_model_name=${14}
 add_valid_in_training_set=true
 lr_decay=true
-warm_up_valid_count=200
+warm_up_valid_count=50
 model_prov_period=20
 
 
 valid_ratio_each_run=200 #$(( total_valid_ratio / repeat_times ))
-bias_flip=true
-method="certain"
+bias_flip=false
+method="cluster_method_three"
 total_valid_sample_count=200
 use_pretrained_model=true
