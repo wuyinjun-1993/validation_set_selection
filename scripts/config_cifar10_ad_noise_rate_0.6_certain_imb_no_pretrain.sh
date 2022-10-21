bf7ae96621ef3fc9e3f1da1058127747ac9d97d1
thuwuyinjun <wuyinjun@seas.upenn.edu>
2022-10-20 21:46:16 -0400

    update

---
+++ scripts/config_cifar10_ad_noise_rate_0.6_certain_imb_no_pretrain.sh
@@ -0,0 +1,27 @@
+imb_factor=200
+
+dataset_name=cifar10
+data_dir="/data6/wuyinjun/valid_set_selections/cifar10_4/"
+save_path_root_dir="/data6/wuyinjun/valid_set_selections/cifar10_4/"
+output_dir="/data6/wuyinjun/valid_set_selections/cifar10_4/"
+gpu_ids=2
+#total_valid_ratio=$6
+repeat_times=10
+port_num=10432
+meta_lr=30
+lr=0.1
+batch_size=128
+test_batch_size=128
+epochs=150
+#cached_model_name=${14}
+add_valid_in_training_set=true
+lr_decay=true
+warm_up_valid_count=1
+model_prov_period=20
+
+
+valid_ratio_each_run=20 #$(( total_valid_ratio / repeat_times ))
+bias_flip=true
+method="certain"
+total_valid_sample_count=20
+use_pretrained_model=true
